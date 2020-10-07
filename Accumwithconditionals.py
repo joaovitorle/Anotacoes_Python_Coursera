a29ca224e989b3c9778bb430c7df163b22be55a9
João Vitor Leite <joao.v.leite33@gmail.com>
2020-10-07 14:15:50 -0300

    Atividades da semana 3

---
+++ Accumwithconditionals.py
@@ -0,0 +1,8 @@
+frase = 'Meu nome é João Vitor'
+
+accum= 0 
+
+for c in frase: 
+    if c != ' ':
+        accum = accum +1 
+print (accum)
---
+++ BooleanandLogicalOperators.py
@@ -0,0 +1,19 @@
+print (5==6) #vai ler e vai retornar se é true or false 
+
+x = 5 
+print (x>5 and x<10)
+
+n= 25
+print (n%2 == 0 or n%3 ==0)
+
+frase= "João é lindo"
+print ('João' in frase)
+
+print ('Alex' not in frase) 
+
+x = 15
+
+if x % 2 == 0:
+    print(x, "is even")
+else:
+    print(x, "is odd") 
